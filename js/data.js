5f55f44c923014310efa4b0c8059a74586720244
foodrip <124919377+foodrip@users.noreply.github.com>
2023-02-11 18:02:44 +0500

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,225 @@
+let albums = [
+    {
+        title: `Гимны`,
+        img: `assets/image-1.jpg`,
+        year: 2022,
+        description: `Описание альбома, прочитав которое, вы просто не сможете себя остановить перед прослушиванием плейлиста`,
+        tracks: [
+            {
+                title: `Гимн России`,
+                author: `Сергей Михалков , Александр Александров`,
+                time: `3:32`,
+                src: `music/gimn_russia.mp3`
+            },
+            {
+                title: `Гимн Украины`,
+                author: `Михаил Вербицкий`,
+                time: `1:45`,
+                src: `music/gimn_ukraine.mp3`
+            },
+            {
+                title: `Гимн Китая`,
+                author: `Не Эр`,
+                time: `0:47`,
+                src: `music/gimn_chine.mp3`
+            },
+            {
+                title: `Гимн США`,
+                author: `Джон Смит`,
+                time: `5:13`,
+                src: `music/gimn-usa.mp3`
+            }
+        ]
+    },
+    {
+        title: `Любовь. Слезы. Движ`,
+        img: `assets/image-2.jpg`,
+        year: 2022,
+        description: `Никто не хочет убираться, но нашу музыку любят все`,
+        tracks: [
+            {
+                title: `Не люблю?`,
+                author: `Анет Сай`,
+                time: `2:31`,
+                src: `music/not love.mp3`
+            },
+            {
+                title: `Благодарности`,
+                author: `Анет Сай`,
+                time: `1:36`,
+                src: `music/blagodarnosti.mp3`
+            },
+            {
+                title: `Лимон`,
+                author: `Анет Сай`,
+                time: `3:01`
+            },
+            {
+                title: `Движ`,
+                author: `Анет Сай`,
+                time: `2:48`
+            },
+            {
+                title: `Тонкий лед`,
+                author: `Анет Сай`,
+                time: `2:17`
+            },
+            {
+                title: `Скоро будет легче`,
+                author: `Анет Сай`,
+                time: `1:55`
+            },
+            {
+                title: `Слезы (Из т/ш "Пацанки")`,
+                author: `Анет Сай`,
+                time: `2:55`
+            },
+            {
+                title: `Как я захочу`,
+                author: `Анет Сай`,
+                time: `2:43`
+            },
+            {
+                title: `Прости`,
+                author: `Анет Сай`,
+                time: `3:20`
+            },
+            {
+                title: `В летних кроссах`,
+                author: `Анет Сай`,
+                time: `2:15`
+            },
+            {
+                title: `Оставаться человеком`,
+                author: `Анет Сай`,
+                time: `3:23`
+            },
+            {
+                title: `Он беден`,
+                author: `Анет Сай`,
+                time: `2:56`
+            }
+        ]
+    },
+    {
+        title: `Miss`,
+        img: `assets/image-3.jpg`,
+        year: 2022,
+        description: `Я просто в тебя втюрилась, вкрашилась как в эту музыку`,
+        tracks: [
+            {
+                title: `Засыпать`,
+                author: `Дора`,
+                time: `5:41`
+            },
+            {
+                title: `Заставь`,
+                author: `Дора`,
+                time: `3:21`
+            },
+            {
+                title: `Нет тебя (feat. ЛСП)`,
+                author: `Дора`,
+                time: `3:43`
+            },
+            {
+                title: `Мама`,
+                author: `Дора`,
+                time: `3:10`
+            },
+            {
+                title: `Стоп-слово`,
+                author: `Дора`,
+                time: `2:47`
+            },
+            {
+                title: `Барбисайз (feat. МЭЙБИ БЭЙБИ)`,
+                author: `Дора`,
+                time: `3:04`
+            },
+            {
+                title: `Я боюсь людей`,
+                author: `Дора`,
+                time: `2:12`
+            },
+            {
+                title: `Бриллианты`,
+                author: `Дора`,
+                time: `2:23`
+            },
+            {
+                title: `Автопилот (feat. Платина)`,
+                author: `Дора`,
+                time: `3:15`
+            },
+            {
+                title: `Розовые волосы`,
+                author: `Дора`,
+                time: `2:32`
+            },
+            {
+                title: `Loverboy`,
+                author: `Дора`,
+                time: `2:37`
+            },
+            {
+                title: `Маленькая леди`,
+                author: `Дора`,
+                time: `2:36`
+            }
+        ]
+    },
+    {
+        title: `Искренний`,
+        img: `assets/image-4.jpg`,
+        year: 2022,
+        description: `Я слышал, то, что эта жизнь прекрасна, но почему мне грустно. Ну-ка, послушаю этот плейлист`,
+        tracks: [
+            {
+                title: `ДНК`,
+                author: `Face`,
+                time: `2:54`
+            },
+            {
+                title: `Через Шторм`,
+                author: `Face`,
+                time: `3:31`
+            },
+            {
+                title: `Домино`,
+                author: `Face`,
+                time: `3:19`
+            },
+            {
+                title: `Независимое Падение`,
+                author: `Face`,
+                time: `2:54`
+            },
+            {
+                title: `Расстояние`,
+                author: `Face`,
+                time: `3:06`
+            },
+            {
+                title: `Пей`,
+                author: `Face`,
+                time: `3:21`
+            },
+            {
+                title: `Статус`,
+                author: `Face`,
+                time: `3:07`
+            },
+            {
+                title: `Истерика`,
+                author: `Face`,
+                time: `3:11`
+            },
+            {
+                title: `Коттон`,
+                author: `Face`,
+                time: `2:41`
+            }
+        ]
+    }
+]
